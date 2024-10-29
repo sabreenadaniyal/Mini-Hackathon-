4c64e8b4fd1cab489a0c01b86df6c0d8706a9923
Sabreena Daniyal <160657530+sabreenadaniyal@users.noreply.github.com>
2024-10-29 23:57:30 +0500

    Update Footer.tsx

--- app/Components/Footer.tsx
+++ app/Components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
             <div className="flex w-full bg-slate-800 h-24 items-center text-white justify-center">
 
                <p>Made by | </p>  
-                <Link className="font-serif font-bold text-center" href="https://www.linkedin.com/in/sabreena-zahid-ali-1343a8198/" target="_blank">
+                <Link className="hover:text-red-300 font-serif font-bold text-center" href="https://www.linkedin.com/in/sabreena-zahid-ali-1343a8198/" target="_blank">
                     SABREENA ZAHID ALI </Link>  
 
                     <div className="text-gray-500 mt-14 -ml-44">
